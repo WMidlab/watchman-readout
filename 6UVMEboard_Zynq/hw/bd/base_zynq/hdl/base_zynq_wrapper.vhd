6120bcd0e8da0100b9ea5e2fec34f9cada976fce
sventurag <salvadorventurag@gmail.com>
2022-01-20 08:42:29 -1000

    stop tracking unnecesary files

--- 6UVMEboard_Zynq/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
+++ 6UVMEboard_Zynq/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
---Date        : Tue Jan  4 05:39:03 2022
+--Date        : Fri Jan 14 10:41:31 2022
 --Host        : idlab2 running 64-bit Ubuntu 20.04.3 LTS
 --Command     : generate_target base_zynq_wrapper.bd
 --Design      : base_zynq_wrapper
